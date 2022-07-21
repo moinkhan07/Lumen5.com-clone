2fd35b74a247f0850e909f303bad15a7ede79111
Shubham Kumar <shubhamk9097@gmail.com>
2022-07-21 20:09:36 +0530

    footer

---
+++ Navbar/navbar.js
@@ -0,0 +1,26 @@
+const button=document.getElementById("learn");
+    const list= document.getElementById("learn_div")
+
+    const button_cr=document.getElementById("create");
+    const list_cr= document.getElementById("create_list")
+
+    list.style.display="none"
+    list_cr.style.display="none"
+
+    button.addEventListener("click",(event)=> {
+        if (list.style.display=="none"){
+        list.style.display="block";
+    }
+    else{
+        list.style.display="none"
+    }
+    })
+
+    button_cr.addEventListener("click",(event_cr)=> {
+        if (list_cr.style.display=="none"){
+        list_cr.style.display="block";
+    }
+    else{
+        list_cr.style.display="none"
+    }
+    })
